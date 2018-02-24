4606e7bc059ac66ebd5b2b9b25a533c819561218
ZhangYeSong <17602105720@163.com>
2018-02-24 21:00:26 +0800

    mybatisGenerator配置

---
+++ src/main/java/com/judy/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.judy.mmall.dao;
+
+import com.judy.mmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
